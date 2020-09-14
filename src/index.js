587f7480dc34620ea5d21bcd6265badc08b6157a
Steve Occhipinti <dev@stevenocchipinti.com>
2020-09-15 01:05:54 +1000

    Make the pin move and add a basic gradient

--- src/index.js
+++ src/index.js
@@ -1,47 +1,65 @@
-var suncalc = require("suncalc")
+import suncalc from "suncalc"
+import dateToDegrees from "./dateToDegrees"
 
-var times = suncalc.getTimes(new Date(), -37.8, 144.9)
+const setProp = (name, value) => {
+  document.documentElement.style.setProperty(name, value)
+}
 
-window.sunrise = times.sunrise
+const setGradient = ({ nadir, ...times }) => {
+  setProp("--nadir-deg", `${dateToDegrees(nadir)}deg`)
+  Object.keys(times).forEach(time =>
+    setProp(
+      `--${time}-deg`,
+      `calc(${dateToDegrees(times[time])}deg - ${dateToDegrees(nadir)}deg)`
+    )
+  )
+}
 
-// sunrise (top edge of the sun appears on the horizon)
-console.log("sunrise             " + times.sunrise)
+const movePin = () => {
+  setProp("--degrees", `${dateToDegrees(new Date())}deg`)
+}
 
-// sunrise ends (bottom edge of the sun touches the horizon)
-console.log("sunriseEnd          " + times.sunriseEnd)
+movePin()
+setInterval(movePin, 60000)
 
-// morning golden hour (soft light, best time for photography.ends
-console.log("goldenHourEnd       " + times.goldenHourEnd)
+const times = suncalc.getTimes(new Date(), -37.8, 144.9)
+setGradient(times)
 
-// solar noon (sun is in the highest position)
-console.log("solarNoon           " + times.solarNoon)
+// Debugging
 
-// evening golden hour starts
-console.log("goldenHour          " + times.goldenHour)
+const descriptions = {
+  nightEnd: "night ends (morning astronomical twilight starts)",
+  nauticalDawn: "nautical dawn (morning nautical twilight starts)",
+  dawn: "dawn (morning nautical twilight ends, morning civil twilight starts)",
+  sunrise: "sunrise (top edge of the sun appears on the horizon)",
+  sunriseEnd: "sunrise ends (bottom edge of the sun touches the horizon)",
+  goldenHourEnd:
+    "morning golden hour (soft light, best time for photography.ends",
+  solarNoon: "solar noon (sun is in the highest position)",
+  goldenHour: "evening golden hour starts",
+  sunsetStart: "sunset starts (bottom edge of the sun touches the horizon)",
+  sunset:
+    "sunset (sun disappears below the horizon, evening civil twilight starts)",
+  dusk: "dusk (evening nautical twilight starts)",
+  nauticalDusk: "nautical dusk (evening astronomical twilight starts)",
+  night: "night starts (dark enough for astronomical observations)",
+  nadir: "nadir (darkest moment of the night, sun is in the lowest position)",
+}
 
-// sunset starts (bottom edge of the sun touches the horizon)
-console.log("sunsetStart         " + times.sunsetStart)
+const dateToTime = date => `${date.getHours()}:${date.getMinutes()}`
 
-// sunset (sun disappears below the horizon, evening civil twilight starts)
-console.log("sunset              " + times.sunset)
-
-// dusk (evening nautical twilight starts)
-console.log("dusk                " + times.dusk)
-
-// nautical dusk (evening astronomical twilight starts)
-console.log("nauticalDusk        " + times.nauticalDusk)
-
-// night starts (dark enough for astronomical observations)
-console.log("night               " + times.night)
-
-// nadir (darkest moment of the night, sun is in the lowest position)
-console.log("nadir               " + times.nadir)
-
-// night ends (morning astronomical twilight starts)
-console.log("nightEnd            " + times.nightEnd)
-
-// nautical dawn (morning nautical twilight starts)
-console.log("nauticalDawn        " + times.nauticalDawn)
-
-// dawn (morning nautical twilight ends, morning civil twilight starts)
-console.log("dawn                " + times.dawn)
+console.table(
+  Object.keys(times)
+    .sort((a, b) => (times[a] > times[b] ? 1 : -1))
+    .reduce(
+      (a, i) => ({
+        ...a,
+        [i]: {
+          time: dateToTime(times[i]),
+          degrees: dateToDegrees(times[i]).toFixed(2),
+          description: descriptions[i],
+        },
+      }),
+      {}
+    )
+)
